ef9d56f14c858cfb1b213a053afe7ca1718928ba
Muratcan Şentürk <mcsnturk@gmail.com>
2021-05-05 22:41:34 +0300

    init

---
+++ src/test/java/com/example/filmList/FilmListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filmList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
